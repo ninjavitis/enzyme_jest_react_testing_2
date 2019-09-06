3628fedbdc3f1e45323fbaa281a2109feb221a10
Peter Mackay <project2501@gmail.com>
2019-09-06 12:08:01 +0900

    implemented congrats component, all tests properly passing

--- src/Congrats.js
+++ src/Congrats.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 /**
  * @function
+ * @param {object} props - react props
  * @returns {JSX.Element}
  */
 
-export default ()=>{
-  return(
-    <div />
-  )
+export default (props) => {
+  if (props.success){
+    return(
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">Congratulations, you guessed the word!</span>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test="component-congrats"></div>
+    )
+  }
 }
