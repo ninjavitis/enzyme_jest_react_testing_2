7142966d114c3528bbd539bc2396045a16c99168
Peter Mackay <project2501@gmail.com>
2019-09-12 18:06:40 +0900

    input prop type testing

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Input =()=>{
+
+const Input =(props,{secretWord})=>{
 
   return(
     <div data-test="input-component"/>
   )
+}
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 }
 
 export default Input
--- src/Components/Input.test.js
+++ src/Components/Input.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme'
-import { findByTestAttr } from "../../Test/TestUtils";
+import { findByTestAttr, checkProps } from "../../Test/TestUtils";
 import Input from './Input'
 
-const setup =()=>{
-  return shallow(<Input />)
+const setup = (secretWord='party') => {
+  return shallow(<Input secretWord={secretWord}/>)
 }
 
 test('it renders', ()=>{
   const wrapper = setup()
   const inputComponent = findByTestAttr(wrapper, 'input-component')
   expect(inputComponent.length).toBe(1)
+})
+
+test ('does not throw warning with expected props',()=>{
+  checkProps(Input, {secretWord: ''})
 })
