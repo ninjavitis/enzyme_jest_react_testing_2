a0ba2fdc2cb8fc10bdc0d7adca6a920d42db09ad
Peter Mackay <project2501@gmail.com>
2019-09-13 10:06:57 +0900

    set up secret word state and state reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import HookActions from './Actions/HookActions'
 import GuessedWords from './GuessedWords'
 import Congrats from './Congrats'
 
+function reducer(state, action){
+  switch(action.type){
+    case "setSecretWord":
+      return {...state, secretWord: action.payload}
+
+    default:
+      throw new Error(`invalid action type ${action.type}`)
+  }
+}
+
+
 function App() {
+  const [state,dispatch] = React.useReducer(reducer,{secretWord:null})
+
+  const setSecretWord = (secretWord) => dispatch({type:'setSecretWord', payload:secretWord})
+
+  React.useEffect(()=>{
+    HookActions.getSecretWord(setSecretWord)
+  },[])
+
   return (
-    <div className="App">
+    <div data-test="component-app" className="App">
       <h1>Jotto</h1>
       <Congrats success={false} />
       <GuessedWords guessedWords={[]} />
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {mount} from 'enzyme'
+import { findByTestAttr } from "../Test/TestUtils";
+import HookActions from "./Actions/HookActions";
+
+const mockGetSecretWord = jest.fn()
+
+const setup = () =>{
+  mockGetSecretWord.mockClear()
+  HookActions.getSecretWord = mockGetSecretWord
+
+  // use effect is not called on shallow due to enzyme-2086
+  return mount(<App />)
+}
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, 'component-app')
+  expect(component.length).toBe(1)
 });
+
+describe('',()=>{
+  test('getSecretWord is called on app mount',()=>{
+    setup()
+    expect(mockGetSecretWord).toHaveBeenCalled()
+  })
+  //test('',()=>{expect()})
+
+})
